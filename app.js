9b8c9b5533badddb7c60c6dfe5a225c7bdbc0375
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-03-10 19:01:04 +0100

    model mongoose + début enregistrer et récupérer des données

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Book = require("./models/Book");
 
 const app = express();
 
@@ -21,32 +22,21 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/books", (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: "Objet créé !",
+    delete req.body._id;
+    // on crée une nouvelle instance du modèle Book
+    const book = new Book({
+        ...req.body, //équivalent de title: req.body.title etc
     });
+    book.save()
+        .then(() => res.status(201).json({ message: "Livre enregistré !" }))
+        .catch((error) => res.status(400).json({ error: error }));
 });
 
 app.get("/api/books", (req, res, next) => {
-    const books = [
-        {
-            _id: "oeihfzeoi",
-            title: "Mon premier objet",
-            description: "Les infos de mon premier objet",
-            imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 4900,
-            userId: "qsomihvqios",
-        },
-        {
-            _id: "oeihfzeomoihi",
-            title: "Mon deuxième objet",
-            description: "Les infos de mon deuxième objet",
-            imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 2900,
-            userId: "qsomihvqios",
-        },
-    ];
-    res.status(200).json(books);
+    // on affiche tous les livres de la BDD
+    Book.find()
+        .then((books) => res.status(200).json(books))
+        .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
