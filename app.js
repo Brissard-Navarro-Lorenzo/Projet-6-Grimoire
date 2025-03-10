ed2479abc06573dc4bc1054654aa4618db91513d
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-03-10 19:44:16 +0100

    modifier et supprimer les données

--- app.js
+++ app.js
@@ -32,6 +32,25 @@ app.post("/api/books", (req, res, next) => {
         .catch((error) => res.status(400).json({ error: error }));
 });
 
+app.put("/api/books/:id", (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Livre modifié !" }))
+        .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Livre supprimé !" }))
+        .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
+    // permet d'afficher le contenu d'un seul livre car on veut que l'id du livre soit le même que le paramètre de requête de l'URL
+    Book.findOne({ _id: req.params.id })
+        .then((book) => res.status(200).json(book))
+        .catch((error) => res.status(404).json({ error }));
+});
+
 app.get("/api/books", (req, res, next) => {
     // on affiche tous les livres de la BDD
     Book.find()
