a6debe7152370d8ef5afe5b58b2c8fde6b63a15c
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-03-13 16:53:05 +0100

    gestion des fichiers

--- routes/stuff.js
+++ routes/stuff.js
@@ -5,9 +5,10 @@ const router = express.Router();
 const stuffCtrl = require("../controllers/stuff");
 
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-router.post("/", auth, stuffCtrl.createBook);
-router.put("/:id", auth, stuffCtrl.modifyBook);
+router.post("/", auth, multer, stuffCtrl.createBook);
+router.put("/:id", auth, multer, stuffCtrl.modifyBook);
 router.delete("/:id", auth, stuffCtrl.deleteBook);
 router.get("/:id", auth, stuffCtrl.getOneBook);
 router.get("/", auth, stuffCtrl.getAllBooks);
